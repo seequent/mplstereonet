ee7d2340966ebaa6efbef68db6a43d2946e2b2ce
Michael Cowie <michael.cowie@seequent.com>
2019-01-14 15:12:31 +1300

    LF-21671 The line that is 90 degree from the polar center will now be straight and not bend slightly

--- mplstereonet/stereonet_axes.py
+++ mplstereonet/stereonet_axes.py
@@ -106,7 +106,6 @@ class StereonetAxes(LambertAxes):
         """
         number = (360.0 / degrees) + 1
         locs = np.linspace(-np.pi, np.pi, number, True)[1:]
-        locs[-1] -= 0.01 # Workaround for "back" gridlines showing.
         self.xaxis.set_major_locator(FixedLocator(locs))
         self._logitude_degrees = degrees
         self.xaxis.set_major_formatter(self.ThetaFormatter(degrees))
